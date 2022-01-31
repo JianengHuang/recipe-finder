94d4420d25bc12b8002d8f14054add6ee5e681f2
Jianeng Huang <jianengh@gmail.com>
2022-01-31 17:10:28 +0100

    Added Diet Filter

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import Axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import Recipe from './components/Recipe';
-import Alert from './components/Alert';
-console.log(process.env);
+
 const App = () => {
   const [inputField, setInputField] = useState([{ ingredient: '' }]);
   const [recipes, setRecipes] = useState([]);
   const [alert, setAlert] = useState('Enter the ingredients you want to use: ');
+  const [defaultShow, setDefaultShow] = useState(false);
+  const [diet, setDiet] = useState({
+    balanced: false,
+    highFiber: false,
+    highProtein: false,
+    lowCarb: false,
+    lowFat: false,
+    lowSodium: false,
+  });
+  let dietString = '';
+  const { balanced, highFiber, highProtein, lowCarb, lowFat, lowSodium } = diet;
 
   const handleChangeInput = (index, event) => {
     const values = [...inputField];
@@ -43,11 +53,37 @@ const App = () => {
     event.preventDefault();
   };
 
+  const handleSubmit = () => {
+    dietString = '';
+    for (let x in diet) {
+      if (eval(x)) {
+        switch (x) {
+          case 'highFiber':
+            x = 'high-fiber';
+            break;
+          case 'highProtein':
+            x = 'high-protein';
+            break;
+          case 'lowCarb':
+            x = 'low-carb';
+            break;
+          case 'lowFat':
+            x = 'low-fat';
+            break;
+          case 'lowSodium':
+            x = 'low-sodium';
+            break;
+        }
+        dietString += `&diet=${x}`;
+      }
+    }
+  };
+
   const getData = async (ingredients) => {
     const API_ID = process.env.REACT_APP_API_ID;
     const API_KEY = process.env.REACT_APP_API_KEY;
     const deviation = '2-3';
-    const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${ingredients}&app_id=${API_ID}&app_key=${API_KEY}&ingr=${deviation}`;
+    const url = `https://api.edamam.com/api/recipes/v2?q=${ingredients}&ingr=${deviation}&app_key=${API_KEY}${dietString}&type=public&app_id=${API_ID}&beta=true`;
     const result = await Axios.get(url).catch(function (error) {
       console.log(error);
     });
@@ -64,7 +100,7 @@ const App = () => {
     <>
       <div className='container'>
         <form>
-          {alert || <Alert />}
+          {alert}
           {inputField.map((inputField, index) => {
             return (
               <div key={index} className='input'>
@@ -91,9 +127,67 @@ const App = () => {
           </button>
         </form>
       </div>
+      <div className='Advanced-Filter'>
+        <form className='diet-form'>
+          <h3 className='filter-tag'>Diet</h3>
+          <label>
+            <input
+              type='checkbox'
+              name='balanced'
+              onChange={() => setDiet({ ...diet, balanced: !balanced })}
+            ></input>
+            Balanced
+          </label>
+          <label>
+            <input
+              type='checkbox'
+              name='high-fiber'
+              onChange={() => setDiet({ ...diet, highFiber: !highFiber })}
+            ></input>
+            High-Fiber
+          </label>
+          <label>
+            <input
+              type='checkbox'
+              name='high-protein'
+              onChange={() => setDiet({ ...diet, highProtein: !highProtein })}
+            ></input>
+            High-Protein
+          </label>
+          <label>
+            <input
+              type='checkbox'
+              name='low-carb'
+              onChange={() => setDiet({ ...diet, lowCarb: !lowCarb })}
+            ></input>
+            Low-Carb
+          </label>
+          <label>
+            <input
+              type='checkbox'
+              name='low-fat'
+              onChange={() => setDiet({ ...diet, lowFat: !lowFat })}
+            ></input>
+            Low-Fat
+          </label>
+          <label>
+            <input
+              type='checkbox'
+              name='low-sodium'
+              onChange={() => setDiet({ ...diet, lowSodium: !lowSodium })}
+            ></input>
+            Low-Sodium
+          </label>
+        </form>
+        <button onClick={handleSubmit}>Apply Changes</button>
+      </div>
+      <label>
+        <input type='checkbox' onClick={() => setDefaultShow(!defaultShow)} />
+        Default Show Ingredients
+      </label>
       <div className='recipes'>
         {recipes !== [] &&
-          recipes.map((recipe) => <Recipe key={uuidv4()} recipe={recipe} />)}
+          recipes.map((recipe) => <Recipe key={uuidv4()} recipe={recipe} defaultShow={defaultShow} />)}
       </div>
     </>
   );
